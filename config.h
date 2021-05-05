d34a6e491d9ac840e1b6638b2d1378b4cb69108a
David García Pérez <david.garcia@weareadaptive.com>
2021-05-05 17:17:19 +0200

    chore: Update default configuration

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack\ Nerd\ Font:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "Hack\ Nerd\ Font:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "Hack\ Nerd\ Font:pixelsize=14:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 /*
